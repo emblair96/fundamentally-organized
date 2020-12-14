7156e5dccc2d6ed5e9625e4c6e38e11c3383fa39
Emily Blair <emilyblair96@gmail.com>
2020-12-13 22:56:58 -0600

    Add working get & post HTTP methods

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const uuid = require("uuid");
 
 const app = express();
 const PORT = 3000;
 
 const notes = [];
 
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,15 +20,29 @@ app.get("/notes", function(req, res) {
 res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.post("/api/notes", function(req, res) {
-  var newNote = req.body;
-  console.log(newNote);
-  notes.push(newNote);
-  res.json(newNote);
+app.get("/api/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/db/db.json"));  
 });
 
-app.get("/api/notes", function(req, res) {
-  res.json(notes);
+app.post("/api/notes", function(req, res) {
+  var newNote = req.body;
+  newNote.id = uuid.v4();
+  console.log(newNote);
+  notes.push(newNote);
+
+  // Add the new note to the db.json file
+  fs.readFile("db/db.json", function(err, notes) {
+    var json = JSON.parse(notes);
+    json.push(newNote);
+    fs.writeFile("db/db.json", JSON.stringify(json), function(err) {
+      err
+      ? console.log(err)
+      : console.log("Note successfully added to file.")
+    });
+  });
+
+  res.json(newNote);
+
 });
 
 app.listen(PORT, function() {
